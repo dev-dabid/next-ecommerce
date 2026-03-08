78a291f01bd9ff8aa06b8da8e0b4a4590b76f435
David Chu <dabidjohn424@gmail.com>
2026-03-08 19:51:13 +0800

    update: tailwind

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function HomePage() {
   return (
-    <div className="max-w-5xl mx-auto">
+    <div className="max-w-5xl mx-auto bg-amber-200 w-full text-7xl">
       <h1 className="text-3xl font-bold mb-4">Welcome to My Store</h1>
       <p className="text-gray-700">Check out our amazing products below.</p>
     </div>
