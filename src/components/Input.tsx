ad23a86b3c41e0a59c87d529aa7386b560aed06d
David Chu <dabidjohn424@gmail.com>
2026-03-12 22:35:02 +0800

    style: update input style

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -2,7 +2,7 @@ import { Search } from "lucide-react";
 
 const Input = () => {
   return (
-    <div className="flex gap-3 p-2 bg-cyan-50 rounded-lg">
+    <div className="flex gap-3 p-2 bg-sky-100 rounded-lg">
       <Search className="text-gray-400" />
       <input
         className="focus:ring-0 outline-none text-gray-400"
