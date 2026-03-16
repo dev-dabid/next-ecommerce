f62142619071fa0687ad7c4ad4c3eb1ee662eead
David Chu <dabidjohn424@gmail.com>
2026-03-16 19:26:49 +0800

    style: update news letter section and new letter input

--- src/app/(home)/NewsLetterInput.tsx
+++ src/app/(home)/NewsLetterInput.tsx
@@ -1,14 +1,14 @@
 const NewsLetterInput = () => {
   return (
     <div className="flex flex-col lg:flex-row gap-2 w-full">
-      <div className="bg-white py-4 px-4 rounded-xl flex-1">
+      <div className="bg-white py-4 px-4 rounded-xl lg:flex-1">
         <input
           className="w-full outline-none"
           type="text"
           placeholder="Enter your email address"
         />
       </div>
-      <button className="bg-gray-900 text-white px-[clamp(1.5rem,2vw,2.5rem)] text-sm rounded-xl font-semibold">
+      <button className="bg-gray-900 text-white px-[clamp(1.5rem,2vw,2.5rem)] text-sm rounded-xl font-semibold py-4">
         Subscribe Now
       </button>
     </div>
--- src/app/(home)/NewsLetterSection.tsx
+++ src/app/(home)/NewsLetterSection.tsx
@@ -2,19 +2,19 @@ import NewsLetterInput from "./NewsLetterInput";
 
 const NewsLetterSection = () => {
   return (
-    <div className="py-20">
+    <div className="py-[clamp()]">
       <div className="max-w-300 mx-auto bg-sky-400 flex justify-center items-center rounded-2xl py-20 flex-col px-5">
-        <h1 className="text-[clamp(1.25rem,2vw,3rem)] font-bold">
+        <h1 className="text-[clamp(1.25rem,6vw,3rem)] font-bold">
           Join the Lumina Collective
         </h1>
-        <p className="mt-5 text-xs max-w-[60ch] text-center">
+        <p className="mt-2 text-[clamp(0.75rem,2vw,1.25rem)] max-w-[60ch] text-center">
           Subscribe to receive first access to new arrivals, private sales, and
           fashion inspiration.
         </p>
         <div className="max-w-180 w-full mt-10">
           <NewsLetterInput />
         </div>
-        <p className="mt-10">
+        <p className="mt-4 text-center text-[clamp(0.75rem,2vw,0.8rem)] text-gray-500">
           By subscribing, you agree to our Privacy Policy ang Terms of Service
         </p>
       </div>
