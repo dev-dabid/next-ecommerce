446d385350b3b4bb529115db7eb2577adfaba1f6
David Chu <dabidjohn424@gmail.com>
2026-04-06 19:38:07 +0800

    feat: add local storage for wishlist and cart

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,13 +1,26 @@
 import { create } from "zustand";
+import { persist, createJSONStorage } from "zustand/middleware"; // Import mo 'to
 import { createProductsSlice } from "./productsSlice";
 import { createCartSlice } from "./cartSlice";
 import { createWishlistSlice } from "./wishlistSlice";
 import type { Store } from "@/types/types";
 
-const useStore = create<Store>((...a) => ({
-  ...createProductsSlice(...a),
-  ...createCartSlice(...a),
-  ...createWishlistSlice(...a),
-}));
+const useStore = create<Store>()(
+  persist(
+    (...a) => ({
+      ...createProductsSlice(...a),
+      ...createCartSlice(...a),
+      ...createWishlistSlice(...a),
+    }),
+    {
+      name: "lumina-storage",
+      storage: createJSONStorage(() => localStorage),
+      partialize: (state) => ({
+        cart: state.cart,
+        wishlist: state.wishlist,
+      }),
+    },
+  ),
+);
 
 export default useStore;
