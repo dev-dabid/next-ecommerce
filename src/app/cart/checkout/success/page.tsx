8f5a25a39ba2f0fb34d8a67c4288448e2d236a0c
David Chu <dabidjohn424@gmail.com>
2026-04-23 18:17:46 +0800

    feat: bento grid layout

--- src/app/cart/checkout/success/page.tsx
+++ src/app/cart/checkout/success/page.tsx
@@ -1,3 +1,28 @@
 export default function Success() {
-  return <div></div>;
+  return (
+    <div className="max-w-300 mx-auto">
+      <div className="flex justify-center my-10">
+        <h1 className="text-6xl font-semibold">THANK YOU FOR YOUR ORDER!</h1>
+      </div>
+
+      <div className="grid grid-cols-1 md:grid-cols-6 grid-rows-3 gap-4 p-6 h-screen">
+        <div className="md:col-span-4 md:row-span-3 bg-blue-500 rounded-3xl p-6 text-white flex flex-col justify-end">
+          <div>
+            <div className="grid"></div>
+          </div>
+        </div>
+
+        <div className="md:col-span-2  bg-orange-400 rounded-3xl p-6 text-white">
+          <h2 className="text-xl font-semibold">Wide Feature</h2>
+        </div>
+
+        <div className="md:col-span-2  bg-zinc-800 rounded-3xl p-6 text-white">
+          <h2 className="text-lg font-medium">Stats</h2>
+        </div>
+        <div className="md:col-span-2  bg-zinc-800 rounded-3xl p-6 text-white">
+          <h2 className="text-lg font-medium">Stats</h2>
+        </div>
+      </div>
+    </div>
+  );
 }
