9d257ef7fbc089327279d8c4cb2a92d997283fa0
David Chu <dabidjohn424@gmail.com>
2026-05-09 19:45:03 +0800

    feat: update prisma schema

--- src/actions/cart.ts
+++ src/actions/cart.ts
@@ -1,7 +1,26 @@
 "user server";
 
 import prisma from "@/lib/prisma";
+import { generateCartKey } from "@/lib/utils/cart";
 
-export async function addToCart() {
-  return await prisma.cartItem.upsert({});
+export async function addToCart(userId: string, productId: string) {
+  return await prisma.cartItem.upsert({
+    where: {
+      userId_productId: {
+        userId: userId,
+        productId: productId,
+      },
+    },
+
+    update: {
+      quantity: {
+        increment: 1,
+      },
+    },
+
+    create: {
+      userId: userId,
+      productId: productId,
+    },
+  });
 }
