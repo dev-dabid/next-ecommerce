25b1d04d8157bc36611802d0c311066048c769fb
David Chu <dabidjohn424@gmail.com>
2026-06-15 18:27:49 +0800

    feat: track orders link

--- src/components/MyUserButton.tsx
+++ src/components/MyUserButton.tsx
@@ -1,18 +1,32 @@
 "use client";
 
 import { UserButton } from "@clerk/nextjs";
+import { Package } from "lucide-react";
 
 const MyUserButton = () => {
+  const links = [
+    {
+      label: "Track orders",
+      href: "trackorders",
+    },
+    {
+      label: "Track orders",
+      href: "trackorders",
+    },
+  ];
+
   return (
     <UserButton>
       <UserButton.MenuItems>
-        {
-          <UserButton.Link
-            label="Track orders"
-            href="fsdf"
-            labelIcon={<span></span>}
-          />
-        }
+        {links.map((item) => {
+          return (
+            <UserButton.Link
+              label={item.label}
+              href={item.href}
+              labelIcon={<Package className="text-gray-500" size={15} />}
+            />
+          );
+        })}
       </UserButton.MenuItems>
     </UserButton>
   );
