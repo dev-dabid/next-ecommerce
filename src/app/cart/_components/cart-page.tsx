336e361de2ee415d9829c6efad82f13df4cd6b08
David Chu <dabidjohn424@gmail.com>
2026-08-13 19:05:43 +0800

    feat: add proceed guard to cart page

--- src/app/cart/_components/cart-page.tsx
+++ src/app/cart/_components/cart-page.tsx
@@ -280,6 +280,7 @@ export function CartPage({ userId, cartProducts }: CartPageProps) {
                   cartItems={cartItems}
                   onNavigate={onNavigate}
                   buttonTitle={"PROCEED TO CHECKOUT"}
+                  isPending={isPending}
                 />
                 <Link
                   className="mt-5 text-sm font-semibold flex justify-center items-center gap-2 text-gray-500 hover:text-gray-700"
